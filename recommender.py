d6eadb339956f7d4f67f3c1ec44bf9ea32ec7435
Aakriti Jain <163650998+aacritea@users.noreply.github.com>
2024-12-25 19:05:41 +0530

    Update recommender.py

--- recommender.py
+++ recommender.py
@@ -1,1 +1,8 @@
+import lenskit.datasets as ds
+import pandas as pd
+data = ds.MovieLens('lab4-recommender-systems/')
 
+print("Successfully installed dataset.")
+
+rows_to_show = 15   
+data.movies.head(rows_to_show)  
