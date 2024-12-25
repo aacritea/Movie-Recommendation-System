9110be5bc4abe7284ac41e2c35b18d8f7d308042
aacritea <aakritijain1412@gmail.com>
2024-12-25 19:10:42 +0530

    Added code

--- recommender.py
+++ recommender.py
@@ -6,3 +6,7 @@ print("Successfully installed dataset.")
 
 rows_to_show = 15   
 data.movies.head(rows_to_show)  
+
+joined_data = data.ratings.join(data.movies['genres'], on='item')
+joined_data = joined_data.join(data.movies['title'], on='item')
+joined_data.head(rows_to_show)
